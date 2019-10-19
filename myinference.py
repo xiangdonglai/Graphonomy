8cc9e60de6bc858a5de7d8baac142fed9888d279
Donglai Xiang <xdl13thu@gmail.com>
2019-10-19 15:26:54 -0400

    myinference

--- myinference.py
+++ myinference.py
@@ -93,7 +93,7 @@ def img_transform(img, transform=None):
     sample = transform(sample)
     return sample
 
-def inference(net, img_path='', output_path='./', output_name='f', use_gpu=True):
+def inferenceWrite(net, img_path='', output_path='./', output_name='f', use_gpu=True):
     '''
 
     :param net:
@@ -174,7 +174,113 @@ def inference(net, img_path='', output_path='./', output_name='f', use_gpu=True)
     cv2.imwrite(output_path+'/{}.seg.png'.format(output_name), results[0, :, :])
 
     end_time = timeit.default_timer()
-    print('time used for the multi-scale image inference' + ' is :' + str(end_time - start_time))
+    print('time used for the multi-scale image inference' + ' is: ' + str(end_time - start_time))
+
+
+def convertImg(imgNp):
+    assert type(imgNp) == np.ndarray
+    _img = Image.fromarray(imgNp)
+    w, h = _img.size
+    if w > 640 or h > 480:
+        s = min(640 / w, 480 / h)
+        w = int(s * w)
+        h = int(s * h)
+        _img = _img.resize((w, h))
+    return _img
+
+
+def inference(net, imgNp, use_gpu=True):
+    '''
+    :param net:
+    :param imgNp: an numpy array of image
+    :return: an 
+    '''
+    # adj
+    adj2_ = torch.from_numpy(graph.cihp2pascal_nlp_adj).float()
+    adj2_test = adj2_.unsqueeze(0).unsqueeze(0).expand(1, 1, 7, 20).cuda().transpose(2, 3)
+
+    adj1_ = Variable(torch.from_numpy(graph.preprocess_adj(graph.pascal_graph)).float())
+    adj3_test = adj1_.unsqueeze(0).unsqueeze(0).expand(1, 1, 7, 7).cuda()
+
+    cihp_adj = graph.preprocess_adj(graph.cihp_graph)
+    adj3_ = Variable(torch.from_numpy(cihp_adj).float())
+    adj1_test = adj3_.unsqueeze(0).unsqueeze(0).expand(1, 1, 20, 20).cuda()
+
+    # multi-scale
+    # scale_list = [1, 0.5, 0.75, 1.25, 1.5, 1.75]
+    scale_list = [1]
+    img = convertImg(imgNp)
+    testloader_list = []
+    testloader_flip_list = []
+    for pv in scale_list:
+        composed_transforms_ts = transforms.Compose([
+            tr.Scale_only_img(pv),
+            tr.Normalize_xception_tf_only_img(),
+            tr.ToTensor_only_img()])
+
+        composed_transforms_ts_flip = transforms.Compose([
+            tr.Scale_only_img(pv),
+            tr.HorizontalFlip_only_img(),
+            tr.Normalize_xception_tf_only_img(),
+            tr.ToTensor_only_img()])
+
+        testloader_list.append(img_transform(img, composed_transforms_ts))
+        # print(img_transform(img, composed_transforms_ts))
+        testloader_flip_list.append(img_transform(img, composed_transforms_ts_flip))
+    # print(testloader_list)
+    start_time = timeit.default_timer()
+    # One testing epoch
+    net.eval()
+    # 1 0.5 0.75 1.25 1.5 1.75 ; flip:
+
+    for iii, sample_batched in enumerate(zip(testloader_list, testloader_flip_list)):
+        inputs, labels = sample_batched[0]['image'], sample_batched[0]['label']
+        inputs_f, _ = sample_batched[1]['image'], sample_batched[1]['label']
+        inputs = inputs.unsqueeze(0)
+        inputs_f = inputs_f.unsqueeze(0)
+        inputs = torch.cat((inputs, inputs_f), dim=0)
+        if iii == 0:
+            _, _, h, w = inputs.size()
+        # assert inputs.size() == inputs_f.size()
+
+        # Forward pass of the mini-batch
+        inputs = Variable(inputs, requires_grad=False)
+
+        with torch.no_grad():
+            if use_gpu >= 0:
+                inputs = inputs.cuda()
+            # outputs = net.forward(inputs)
+            outputs = net.forward(inputs, adj1_test.cuda(), adj3_test.cuda(), adj2_test.cuda())
+            outputs = (outputs[0] + flip(flip_cihp(outputs[1]), dim=-1)) / 2
+            outputs = outputs.unsqueeze(0)
+
+            if iii > 0:
+                outputs = F.upsample(outputs, size=(h, w), mode='bilinear', align_corners=True)
+                outputs_final = outputs_final + outputs
+            else:
+                outputs_final = outputs.clone()
+    ################ plot pic
+    predictions = torch.max(outputs_final, 1)[1]
+    results = predictions.cpu().numpy()
+    vis_res = decode_labels(results)
+
+    """
+    parsing_im = Image.fromarray(vis_res[0])
+    import matplotlib.pyplot as plt
+    plt.subplot(1, 2, 1)
+    plt.imshow(img)
+    plt.subplot(1, 2, 2)
+    plt.imshow(parsing_im)
+    plt.show()
+    output_path = '/home/donglaix/Desktop/'
+    parsing_im.save(output_path+'/vis.png')
+    img.save(output_path+'/img.png')
+    # cv2.imwrite(output_path+'/{}.seg.png'.format(output_name), results[0, :, :])
+    """
+
+    end_time = timeit.default_timer()
+    print('time used for the multi-scale image inference is: ' + str(end_time - start_time))
+    return results[0, :, :]
 
 
 def inference_batch(sourcemodel, imgPaths):
@@ -190,7 +296,17 @@ def inference_batch(sourcemodel, imgPaths):
         print('Running Segmentation on {}: No. {}/{}'.format(imgpath, i + 1, n))
         dirname, basename = os.path.split(imgpath)
         basename = '.'.join(basename.split('.')[:-1])
-        inference(net, imgpath, dirname, basename)
+        inferenceWrite(net, imgpath, dirname, basename)
+
+
+def getModel(sourcemodel):
+    net = deeplab_xception_transfer.deeplab_xception_transfer_projection_savemem(n_classes=20,
+                                                                                 hidden_layers=128,
+                                                                                 source_classes=7, )    
+    x = torch.load(sourcemodel)
+    net.load_source_model(x)
+    net.cuda()
+    return net
 
 
 if __name__ == '__main__':
